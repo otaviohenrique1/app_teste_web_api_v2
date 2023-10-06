160783a9772336c41bb21121e854942228a08d3c
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-10-06 20:46:02 -0300

    commit 6

--- src/pages/TelaRegraDeTres.tsx
+++ src/pages/TelaRegraDeTres.tsx
@@ -8,30 +8,32 @@ import * as yup from 'yup';
 import { TextInput } from 'react-native';
 
 const validationSchema = yup.object().shape({
-  campoA: yup.number().required('Vazio'),
-  campoB: yup.number().required('Vazio'),
-  campoC: yup.number().required('Vazio'),
-  campoX: yup.string().required('Vazio'),
+  campoA: yup.string().required('Vazio'),
+  campoB: yup
+    .number()
+    .moreThan(0, "Vazio")
+    .required('Vazio'),
+  // campoC: yup.number().required('Vazio'),
+  // campoX: yup.string().required('Vazio'),
 });
 
 interface FormTypes {
-  campoA: number;
+  campoA: string;
   campoB: number;
-  campoC: number;
-  campoX: string;
+  // campoC: number;
+  // campoX: string;
 }
 
 const valoresIniciais: FormTypes = {
-  campoA: 0,
+  campoA: "",
   campoB: 0,
-  campoC: 0,
-  campoX: ''
+  // campoC: 0,
+  // campoX: ''
 };
 
 export function TelaRegraDeTres() {
   function onSubmitForm(values: FormTypes) {
-    Alert.alert(values.campoA.toString());
-    
+    Alert.alert(values.campoA);
   }
 
   return (
@@ -50,16 +52,27 @@ export function TelaRegraDeTres() {
           {({ handleChange, handleBlur, handleSubmit, setFieldValue, resetForm, values, errors }) => (
             <View style={{ margin: 16 }}>
               <View style={{ marginBottom: 16 }}>
-                <Text>Campo 1</Text>
                 <TextInput
                   onChangeText={handleChange('campoA')}
                   onBlur={handleBlur('campoA')}
-                  value={(values.campoA === 0) ? "" : values.campoA.toString()}
+                  value={values.campoA}
+                  // value={(values.campoA === 0) ? "" : values.campoA.toString()}
                   style={[styles.input]}
                   placeholder="Campo 1"
                   keyboardType="numeric"
                 />
                 {errors.campoA && <Text style={{ color: "red" }}>{errors.campoA}</Text>}
+              </View>
+              <View style={{ marginBottom: 16 }}>
+                <TextInput
+                  onChangeText={handleChange('campoB')}
+                  onBlur={handleBlur('campoB')}
+                  value={(values.campoB === 0) ? "" : values.campoB.toString()}
+                  style={[styles.input]}
+                  placeholder="Campo 1"
+                  keyboardType="numeric"
+                />
+                {errors.campoB && <Text style={{ color: "red" }}>{errors.campoB}</Text>}
               </View>
               <Button title="Gerar" color="blue" onPress={() => handleSubmit()} />
               <Button title="Limpar" color="red" onPress={() => resetForm()} />
